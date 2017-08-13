b33b8ce4dcbf40deda17efd556154a204d200aad
Gabriel Siedler <gabrielsiedler@gmail.com>
2017-08-13 15:30:35 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+
+import Loader from './src/Loader/Loader.jsx';
+
+class App extends Component {
+  render() {
+    <Loader />
+  }
+}
+
+export default App;
